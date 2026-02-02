4395fdc3cc29dedb3de09844ccf40e29854514ca
mydimons <mydimons.kb@gmail.com>
2026-02-02 15:38:23 -0800

    tile map stuffs

---
+++ block.js
@@ -0,0 +1,12 @@
+class Block {
+    constructor(image, x, y, width, height) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.startX = x;
+        this.startY = y;
+    }
+}
